27f0f6f94c40b10164ff02d3f1376a1b29e9f32d
Sergey <krockssb@gmail.com>
2018-01-30 17:46:07 +0200

    +new task

--- lesson.py
+++ lesson.py
@@ -62,30 +62,69 @@ import pandas
 #
 # print('Max mean is', maxMean, 'with neibours', maxNeib)  # printing best result
 ##############################################################################
-from sklearn.datasets import load_boston
-from sklearn.preprocessing import scale
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-import numpy
+# from sklearn.datasets import load_boston
+# from sklearn.preprocessing import scale
+# from sklearn.neighbors import KNeighborsRegressor
+# from sklearn.model_selection import cross_val_score
+# from sklearn.model_selection import KFold
+# import numpy
+#
+# boston = load_boston()
+# data = boston['data']
+# target = boston['target']
+#
+# data = scale(data)
+#
+# steps = numpy.linspace(1.0, 10.0, num=200)
+#
+# minError = -100
+# iError = 0
+# for i in steps:
+#     nbrs = KNeighborsRegressor(n_neighbors=5, weights='distance', p=i)
+#     kf = KFold(n_splits=5, shuffle=True, random_state=42)
+#     cvs = cross_val_score(estimator=nbrs, cv=kf, X=data, y=target, scoring='neg_mean_squared_error')
+#     result = cvs.mean().round(2)
+#     print(result)
+#     if result > minError:
+#         minError = result
+#         iError = i
+# print('MinError is ', minError, 'and i is', iError)
 
-boston = load_boston()
-data = boston['data']
-target = boston['target']
-
-data = scale(data)
-
-steps = numpy.linspace(1.0, 10.0, num=200)
-
-minError = -100
-iError = 0
-for i in steps:
-    nbrs = KNeighborsRegressor(n_neighbors=5, weights='distance', p=i)
-    kf = KFold(n_splits=5, shuffle=True, random_state=42)
-    cvs = cross_val_score(estimator=nbrs, cv=kf, X=data, y=target, scoring='neg_mean_squared_error')
-    result = cvs.mean().round(2)
-    print(result)
-    if result > minError:
-        minError = result
-        iError = i
-print('MinError is ', minError, 'and i is', iError)
+# import pandas
+# from sklearn.linear_model import Perceptron
+# from sklearn.metrics import accuracy_score
+# from sklearn.preprocessing import StandardScaler
+#
+# train = pandas.read_csv('perceptron-train.csv', header=None)
+# test = pandas.read_csv('perceptron-test.csv', header=None)
+#
+#
+# X_train = train[train.columns[1:4]]
+# y_train = train[train.columns[0]]
+#
+# X_test = test[test.columns[1:4]]
+# y_test = test[test.columns[0]]
+#
+# clf = Perceptron(random_state=241)
+# clf.fit(X_train, y_train)
+#
+# predictions = clf.predict(X_test)
+#
+# scaler = StandardScaler()
+#
+# X_train_scaled = scaler.fit_transform(X_train)
+# X_test_scaled = scaler.transform(X_test)
+#
+# clf2 = Perceptron(random_state=241)
+# clf2.fit(X_train_scaled, y_train)
+#
+# predictions_scaled = clf2.predict(X_test_scaled)
+#
+#
+# result_not_normalized = accuracy_score(y_test, predictions)
+# result_normalized = accuracy_score(y_test, predictions_scaled)
+#
+# # print(predictions)
+# print('Not normalized result of accuracy on test dataset is ', result_not_normalized)
+# print('Normalized result of accuracy on test dataset is ', result_normalized)
+# print('Result is ', (result_normalized - result_not_normalized).round(3))
