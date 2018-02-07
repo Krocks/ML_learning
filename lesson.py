541b88091964a3fe18ec98ee288b51700bc1a78e
Sergey <krockssb@gmail.com>
2018-02-07 19:06:11 +0200

    working

--- lesson.py
+++ lesson.py
@@ -180,57 +180,92 @@
 # # with open("q1.txt", "w") as output:
 # #     output.write('%s' % (" ".join(sorted(words))))
 # ######################################################################################
-import pandas
-import numpy as np
-from sklearn.metrics import roc_auc_score
-from scipy.spatial import distance
-
-def split(data):
-    X = data.iloc[:,1:].values
-    y = data.iloc[:,0].values
-    return X, y
-
-def sigmoid(X, w):
-    return 1 / (1 + np.exp(-np.dot(X, w)))
-
-def cost(X, y, w, C):
-    sum = 0
-    n = X.shape[0]
-    m = X.shape[1]
-    for i in range(n):
-        sum += np.log(1 + np.exp(-y[i] * np.dot(X[i], w)))
-    reg = C * (w ** 2).sum() / m
-    cost = sum / np.double(n) + reg
-    return cost
-
-def train(X, y, k, C):
-    n = X.shape[0]
-    m = X.shape[1]
-    w = np.zeros(m)
-    c = cost(X, y, w, C)
-    threshold = 1e-5
-    for iteration in range(10000):
-        new_w = np.zeros(m)
-        for j in range(m):
-            sum = 0
-            for i in range(n):
-                sum += y[i] * X[i, j] * (1 - 1 / (1 + np.exp(-y[i] * np.dot(X[i], w))))
-            new_w[j] = w[j] + k * sum / np.double(n) - k * C * w[j]
-        new_cost = cost(X, y, new_w, C)
-        if distance.euclidean(w, new_w) <= threshold:
-            return new_w
-        c = new_cost
-        w = new_w
-    return w
-
-data = pandas.read_csv('data-logistic.csv', header=None)
-X, y = split(data)
-k = 0.1
-score = roc_auc_score(y, sigmoid(X, train(X, y, k, C = 0)))
-score_reg = roc_auc_score(y, sigmoid(X, train(X, y, k, C = 10)))
-
-print("SCORE", score)
-print("SCORE REG", score_reg)
+# import pandas
+# import numpy as np
+# from sklearn.metrics import roc_auc_score
+# from scipy.spatial import distance
+#
+# def split(data):
+#     X = data.iloc[:,1:].values
+#     y = data.iloc[:,0].values
+#     return X, y
+#
+# def sigmoid(X, w): # for roc_auc ~ qality score
+#     return 1 / (1 + np.exp(-np.dot(X, w)))
+#
+# def cost(X, y, w, C):
+#     sum = 0
+#     n = X.shape[0]
+#     m = X.shape[1]
+#     for i in range(n):
+#         sum += np.log(1 + np.exp(-y[i] * np.dot(X[i], w)))
+#     reg = C * (w ** 2).sum() / m
+#     cost = sum / np.double(n) + reg
+#     return cost
+#
+# def train(X, y, k, C):
+#     n = X.shape[0]
+#     m = X.shape[1]
+#     w = np.zeros(m)
+#     c = cost(X, y, w, C)
+#     threshold = 1e-5
+#     for iteration in range(10000):
+#         new_w = np.zeros(m)
+#         for j in range(m):
+#             sum = 0
+#             for i in range(n):
+#                 sum += y[i] * X[i, j] * (1 - 1 / (1 + np.exp(-y[i] * np.dot(X[i], w))))
+#             new_w[j] = w[j] + k * sum / np.double(n) - k * C * w[j]
+#         new_cost = cost(X, y, new_w, C)
+#         if distance.euclidean(w, new_w) <= threshold:
+#             return new_w
+#         c = new_cost
+#         w = new_w
+#     return w
+#
+# data = pandas.read_csv('data-logistic.csv', header=None)
+# X, y = split(data)
+# k = 0.1
+# score = roc_auc_score(y, sigmoid(X, train(X, y, k, C = 0)))
+# score_reg = roc_auc_score(y, sigmoid(X, train(X, y, k, C = 10)))  # C - коефициент регуляризации
+#
+# print("SCORE", score.round(3))
+# print("SCORE REG", score_reg.round(3))
 # with open("q1.txt", "w") as output:
 #     output.write('%.3f %.3f' % (score, score_reg))
 
+import pandas
+import sklearn.metrics
+
+data = pandas.read_csv('classification.csv')
+
+
+def sort():
+
+    t = data[data['true'] == data['pred']]['pred']
+    tp = t.sum()
+    tn = t.size - tp
+
+    f = data[data['true'] != data['pred']]['pred']
+    fp = f.sum()
+    fn = f.size - fp
+
+    print('%d %d %d %d' % (tp, fp, fn, tn))
+
+#
+# def count():
+#     print('Accuracy is', (sklearn.metrics.accuracy_score(y_true=data['true'], y_pred=data['pred'])).round(2))
+#     print('Precious is', (sklearn.metrics.precision_score(y_true=data['true'], y_pred=data['pred'])).round(2))
+#     print('Recall is', (sklearn.metrics.recall_score(y_true=data['true'], y_pred=data['pred'])).round(2))
+#     print('F-Value is', (sklearn.metrics.f1_score(y_true=data['true'], y_pred=data['pred'])).round(2))
+
+
+sort()
+data.plot.hist()
+# count()
+# print(data)
+
+# fit_data = pandas.read_csv('scores.csv')
+# print(fit_data)
+# print(data[0:1])
+# print(data.shape[1])
