607b4de5fc3d3b1b641bca496f371829e27d5af6
Sergey <krockssb@gmail.com>
2018-02-20 16:28:58 +0200

    working commit

--- current.py
+++ current.py
@@ -496,24 +496,105 @@ import pandas
 # rf_score = log_loss(y_test, rf_pred)
 # print('Question 3', rf_score.round(2))
 
-from skimage.io import imread
-import pandas as pd
+# from skimage.io import imread
+# import pandas as pd
+# import numpy as np
+# import skimage
+# import pylab
+# from sklearn.cluster import KMeans
+#
+# image = imread('parrots.jpg')
+#
+# float_data = skimage.img_as_float(image)
+# w, h, d = float_data.shape
+# float_data = np.reshape(a=float_data, newshape=(w * h, d))
+# data = pd.DataFrame(data=float_data, columns=['R', 'G', 'B'])
+#
+# kmean = KMeans(init='k-means++', random_state=241)
+# cluster = kmean.fit_predict(data)
+# data['cluster'] = cluster
+#
+# means = data.groupby('cluster').mean()
+# median = data.groupby('cluster').median()
+#
+# mean_data = means.iloc[data['cluster'], :]
+# mean_data = mean_data[['R', 'G', 'B']]  # cut off cluster
+#
+# median_data = median.iloc[data['cluster'], :]
+# median_data = median_data[['R', 'G', 'B']]  # cut off cluster
+#
+# means = data.groupby('cluster').mean().values
+# mean_pixels = [means[c] for c in data['cluster'].values]
+# mean_image = np.reshape(mean_pixels, (w, h, d))
+# pylab.imsave('mean.jpg', mean_image)
+#
+# medians = data.groupby('cluster').median().values
+# median_pixels = [medians[c] for c in data['cluster'].values]
+# median_image = np.reshape(median_pixels, (w, h, d))
+# pylab.imsave('median.jpg', median_image)
+
+import math
+import pandas
 import numpy as np
-import skimage
-import pylab
+from skimage import img_as_float
+from skimage.io import imread, imsave
 from sklearn.cluster import KMeans
 
-image = imread('parrots.jpg')
+import sys
+sys.path.append("..")
 
-float_data = skimage.img_as_float(image)
+# 1. Загрузите картинку parrots.jpg. Преобразуйте изображение, приведя все значения в интервал от 0 до 1.
+# Для этого можно воспользоваться функцией img_as_float из модуля skimage.
 
-float_data = np.reshape(a=float_data, newshape=(474*713, 3))
-data = pd.DataFrame(data=float_data, columns=['R', 'G', 'B'])
+image = img_as_float(imread('parrots.jpg'))
+w, h, d = image.shape
 
-print(data)
-# print(float_data.shape)
-kmean = KMeans(init='k-means++', random_state=241)
-cluster = kmean.fit(data)
+# 2. Создайте матрицу объекты-признаки: характеризуйте каждый пиксель тремя координатами - значениями интенсивности
+# в пространстве RGB.
 
-# pylab.imshow(image)
-# pylab.show()
+pixels = pandas.DataFrame(np.reshape(image, (w*h, d)), columns=['R', 'G', 'B'])
+
+# 3. Запустите алгоритм K-Means с параметрами init='k-means++' и random_state=241. После выделения кластеров все
+# пиксели, отнесенные в один кластер, попробуйте заполнить двумя способами: медианным и средним цветом по кластеру.
+
+
+def cluster(pixels, n_clusters=8):
+    print ('Clustering: ' + str(n_clusters))
+
+    pixels = pixels.copy()
+    model = KMeans(n_clusters=n_clusters, init='k-means++', random_state=241)
+    pixels['cluster'] = model.fit_predict(pixels)
+
+    means = pixels.groupby('cluster').mean().values
+    mean_pixels = [means[c] for c in pixels['cluster'].values]
+    mean_image = np.reshape(mean_pixels, (w, h, d))
+    imsave('parrots_' + str(n_clusters) + '.jpg', mean_image)
+
+    medians = pixels.groupby('cluster').median().values
+    median_pixels = [medians[c] for c in pixels['cluster'].values]
+    median_image = np.reshape(median_pixels, (w, h, d))
+    imsave('parrots_' + str(n_clusters) + '.jpg', median_image)
+
+    return mean_image, median_image
+
+
+# 4. Измерьте качество получившейся сегментации с помощью метрики PSNR. Эту метрику нужно реализовать
+# самостоятельно (см. определение).
+
+
+def psnr(image1, image2):
+    mse = np.mean((image1 - image2) ** 2)
+    return 10 * math.log10(float(1) / mse)
+
+
+# 5. Найдите минимальное количество кластеров, при котором значение PSNR выше 20 (можно рассмотреть
+# не более 20кластеров). Это число и будет ответом в данной задаче.
+
+for n in range(1, 21):
+    mean_image, median_image = cluster(pixels, n)
+    psnr_mean, psnr_median = psnr(image, mean_image), psnr(image, median_image)
+    print (psnr_mean, psnr_median)
+
+    if psnr_mean > 20 or psnr_median > 20:
+        print(1, n)
+        break
