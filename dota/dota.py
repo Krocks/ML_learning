313adaf163c6089dfccd202c5876de95941b2e03
Sergey <krockssb@gmail.com>
2018-02-23 16:55:05 +0200

    started

--- dota/dota.py
+++ dota/dota.py
@@ -1,4 +1,10 @@
 import pandas
 
 data = pandas.read_csv('features.csv')
-print(data)
+# for i in (0, data.columns.size):
+#     print('Columns is ', data.columns[i])
+#     print('Value count is', data.columns[i].value_counts)
+#     print('--------------------------------------------')
+print(data.isnull().sum())
+# with open("NaN.txt", "w") as output:
+#     output.write(str(data.isnull().sum()))
