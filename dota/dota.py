0a02a82356f5bffffdafb0682af35f72e3993104
sergey.bondarenko <sergey.bondarenko@quadecco.com>
2018-02-27 01:13:10 +0200

    working

--- dota/dota.py
+++ dota/dota.py
@@ -1,9 +1,11 @@
 import pandas
+import time
+import datetime
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import KFold, cross_val_score
 
 # Считайте таблицу с признаками из файла features.csv с помощью кода, приведенного выше. Удалите признаки,
 # связанные с итогами матча (они помечены в описании данных как отсутствующие в тестовой выборке).
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.model_selection import KFold, cross_val_score
 
 data = pandas.read_csv('features.csv')
 result_columns_names = ['duration', 'radiant_win', 'tower_status_radiant',
@@ -37,12 +39,23 @@ data = data.fillna(0)  # TODO check another filling of NaN
 # настраивались классификаторы? Достигнут ли оптимум на испытанных значениях параметра n_estimators, или же качество,
 #  скорее всего, продолжит расти при дальнейшем его увеличении?
 
-trees_count = [10, 20, 30]
+trees_count = [10, 20, 30, 100]
 for index, trees in enumerate(trees_count):
-    clf = GradientBoostingClassifier(verbose=1, n_estimators=trees)
+    start_time = datetime.datetime.now()
+    clf = GradientBoostingClassifier(verbose=0, n_estimators=trees, max_depth=2, learning_rate=2)
     clf.fit(data, result['radiant_win'])
     kf = KFold(n_splits=5, shuffle=True)
-    cvs = cross_val_score(estimator=clf, cv=kf, X=data, y=result['radiant_win'])
+    cvs = cross_val_score(estimator=clf, cv=kf, X=data, y=result['radiant_win'], scoring='roc_auc')
     print('Trees = ', trees, 'Cross value score = ', cvs.mean())
+    print('Time elapsed:', datetime.datetime.now() - start_time)
+
+# Trees =  10 Cross value score =  0.6645850512249734
+# Time elapsed: 0:00:32.825878
+# Trees =  20 Cross value score =  0.6812031672749024
+# Time elapsed: 0:01:01.634525
+# Trees =  30 Cross value score =  0.6901283769279053
+# Time elapsed: 0:01:28.898085
+# Trees =  100 Cross value score =  0.7062319153700038
+# Time elapsed: 0:04:45.149309
 
 # print(data['match_id'].value_counts())
